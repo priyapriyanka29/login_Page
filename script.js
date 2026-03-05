d8614ff12c07d212ae53172082b327654d4f28b1
Priyanka R <priyapriyankar4@gmail.com>
2026-03-05 14:34:12 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function validateForm(){
+
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
+
+if(username === "" || password === ""){
+alert("Please fill all fields");
+return false;
+}
+
+alert("Login Successful!");
+
+return true;
+
+}
